904fde922762951b5285d0c3d69f9d5420d011d0
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2022-11-25 15:18:31 +0100

    added History Back Up Feature and Style Modifications

--- script.js
+++ script.js
@@ -6,46 +6,83 @@ const angel = document.querySelector("#angel");
 const colorsHexText = document.querySelectorAll(".color");
 const cssCode = document.querySelector(".css-code");
 const modal = document.querySelector(".modal");
+// History VARS 
+let openHistoryBtn = document.querySelector(".open-icon");
+let closeHistoryBtn = document.querySelector(".close-icon");
+let historyMenu = document.querySelector(".history");
+const gradientHistory = [];
 
-window.addEventListener("load", ()=>{
-    let firstClr = localStorage.getItem("color1");
-    let secondClr = localStorage.getItem("color2");
-    let angelValue = localStorage.getItem("angel");
-    for(let i = 0; i < clrElements.length; i++) {
-        clrElements[0].value = firstClr;
-        clrElements[1].value = secondClr;
-        angel.value = angelValue;
-        clrElements.forEach(element => {
-            containerWrapper.style.background = `linear-gradient(${angelValue}deg, ${firstClr}, ${secondClr})`;
-            cssCode.innerText = `background: linear-gradient(${angelValue}deg, ${firstClr}, ${secondClr});`;
-            cssCode.style.background = `linear-gradient(${angelValue}deg, ${firstClr}, ${secondClr})`;
-            element.addEventListener("change", ()=>{
-                localStorage.setItem("color1", clrElements[0].value);
-                localStorage.setItem("color2", clrElements[1].value);
-                localStorage.setItem("angel", angel.value);
-                containerWrapper.style.background = `linear-gradient(${angel.value}deg, ${clrElements[0].value}, ${clrElements[1].value})`;
-                cssCode.innerText = `background: linear-gradient(${angel.value}deg, ${clrElements[0].value}, ${clrElements[1].value});`;
-                cssCode.style.background = `linear-gradient(${angel.value}deg, ${clrElements[0].value}, ${clrElements[1].value})`;
-            });
+
+displayHistory();
+let firstClr = localStorage.getItem("color1");
+let secondClr = localStorage.getItem("color2");
+let angelValue = localStorage.getItem("angel");
+for(let i = 0; i < clrElements.length; i++) {
+    clrElements[0].value = firstClr;
+    clrElements[1].value = secondClr;
+    angel.value = angelValue;
+    clrElements.forEach(element => {
+        containerWrapper.style.background = `linear-gradient(${angelValue}deg, ${firstClr}, ${secondClr})`;
+        cssCode.innerText = `background: linear-gradient(${angelValue}deg, ${firstClr}, ${secondClr});`;
+        cssCode.style.background = `linear-gradient(${angelValue}deg, ${firstClr}, ${secondClr})`;
+        element.addEventListener("change", ()=>{
+            localStorage.setItem("color1", clrElements[0].value);
+            localStorage.setItem("color2", clrElements[1].value);
+            localStorage.setItem("angel", angel.value);
+            containerWrapper.style.background = `linear-gradient(${angel.value}deg, ${clrElements[0].value}, ${clrElements[1].value})`;
+            cssCode.innerText = `background: linear-gradient(${angel.value}deg, ${clrElements[0].value}, ${clrElements[1].value});`;
+            cssCode.style.background = `linear-gradient(${angel.value}deg, ${clrElements[0].value}, ${clrElements[1].value})`;
         });
+    });
+}
+
+for (let i = 0; i < colorsHexText.length; i++) {
+    let colorHex = colorsHexText[i];
+    colorHex.addEventListener("click", copyColorAttribute);
+}
+
+function copyColorAttribute(e) {
+    let hex = e.target;
+    let hexValue = hex.innerText;
+    navigator.clipboard.writeText(hexValue);
+    addGradientToLocalStorage(hexValue);
+    if(modal.classList.contains("active")) {
+        modal.classList.remove("active");
+    }else {
+        modal.classList.add("active");
     }
-    
-    for (let i = 0; i < colorsHexText.length; i++) {
-        let colorHex = colorsHexText[i];
-        colorHex.addEventListener("click", copyColorAttribute);
+    setInterval(()=>{
+        modal.classList.remove("active");
+    }, 3500);
+}
+
+// ! History Menu
+
+const openHistoryMenu = ()=>{
+    historyMenu.classList.add("active-menu");
+}
+const closeHistoryMenu = ()=>{
+    historyMenu.classList.remove("active-menu");
+}
+
+openHistoryBtn.addEventListener("click", openHistoryMenu);
+closeHistoryBtn.addEventListener("click", closeHistoryMenu);
+
+function addGradientToLocalStorage(value) {
+    if(localStorage.getItem('history') == null) {
+        localStorage.setItem('history', '[]');
     }
-    
-    function copyColorAttribute(e) {
-        let hex = e.target;
-        let hexValue = hex.innerText;
-        navigator.clipboard.writeText(hexValue);
-        if(modal.classList.contains("active")) {
-            modal.classList.remove("active");
-        }else {
-            modal.classList.add("active");
+    let oldHistory = JSON.parse(localStorage.getItem('history'));
+    oldHistory.push(value);
+    localStorage.setItem('history', JSON.stringify(oldHistory));
+}
+
+function displayHistory() {
+    if(localStorage.getItem('history') != null) {
+        let historyCode = historyMenu.lastElementChild;
+        let historyData = JSON.parse(localStorage.getItem('history'));
+        for(let i = 0; i < historyData.length; i++) {
+            historyCode.innerHTML += `<h3>${historyData[i]}</h3>`;
         }
-        setInterval(()=>{
-            modal.classList.remove("active");
-        }, 3500);
     }
-});
+}
